01ab97da4e4a7041cd0c381d0452c68bf6318828
iroid2 <104001652+iroid2@users.noreply.github.com>
2022-11-08 14:29:43 +0300

    added TYPE JS

--- script.js
+++ script.js
@@ -32,3 +32,4 @@ function cb(entries){
     // }
     observer.observe(sectn3)
  })
+ 
---
+++ type.js
@@ -0,0 +1,10 @@
+var typed = new Type('.type', {
+    strings:
+     ['Web Developer based ',
+     'Web Designer based ',
+     'in Kampala'
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+  });
